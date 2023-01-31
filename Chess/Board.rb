931aa4001718c59642a340e9c60cef60f80a6d2d
Kelly Zhou <yinglinzhou0@gmail.com>
2023-01-31 16:45:50 -0500

    move piece done

--- Chess/Board.rb
+++ Chess/Board.rb
@@ -23,11 +23,26 @@ class Board
     @grid[x][y]
   end
 
-  def [](position, value)
+  def []=(position, value)
     x, y = position
     @grid[x][y] = value
   end
 
+  def move_piece(start_pos, end_pos)
+    start_x, start_y = start_pos
+    end_x, end_y = end_pos
+    raise "No piece at start position" if !valid_pos?(start_pos) || @grid[start_x][start_y] == nil
+    raise "Cannot move to end position" if !valid_pos?(end_pos) || @grid[end_x][end_y] != nil
+    piece = @grid[start_x][start_y]
+    @grid[end_x][end_y] = piece
+    @grid[start_x][start_y] = nil
+  end
 
-  
+  def valid_pos?(position)
+    x, y = position
+    return true if x >= 0 && x < 8 && y >= 0 && y < 8 
+    false
+  end
+
+
 end
